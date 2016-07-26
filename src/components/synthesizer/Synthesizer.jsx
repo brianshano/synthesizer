16aecf537077cf4304bc9f2146268217180d0f49
Ben Taylor <ben.taylor@powerreviews.com>
2016-07-25 21:41:59 -0500

    adding fuller sound to the synth

--- src/components/synthesizer/Synthesizer.jsx
+++ src/components/synthesizer/Synthesizer.jsx
@@ -10,12 +10,25 @@ require('./Synthesizer.scss');
 class Synthesizer extends React.Component {
   constructor(props) {
     super(props);
-    this.envelope = new Tone.AmplitudeEnvelope({
-      attack : 0.1,
-      decay : 0.2,
-      sustain : 1.0,
-      release : 0.8
+    this.freeverb = new Tone.Freeverb({
+      roomSize: 0.9,
+      dampening: 3000
     }).toMaster();
+    this.chorus = new Tone.Chorus({
+      frequency: 0.5,
+      delayTime: 1.5,
+      depth: 0.7,
+      feedback: 0.1,
+      type: 'sine',
+      spread: 280
+    }).toMaster();
+    this.envelope = new Tone.AmplitudeEnvelope({
+      attack: 0.1,
+      decay: 0.2,
+      sustain: 1.0,
+      release: 0.8
+    }).toMaster().connect(this.freeverb).connect(this.chorus);
+
 
     this.startNote = this.startNote.bind(this);
     this.stopNote = this.stopNote.bind(this);
@@ -45,7 +58,7 @@ class Synthesizer extends React.Component {
           detune={ this.props.detuneAmount }
           waveform={ this.props.waveFormType }
           volume={ this.props.volumeAmount }
-          type={ 'sine' }
+          type={ 'sawtooth' }
           envelope={this.envelope}
           notePlaying={this.props.notePlaying}>
           <section>testing the Oscillator</section>
